1079af31dc40c4c0c31c4a67f75fc910b7ffd792
@kenab.kc <kenab.kc@islingtoncollege.edu.np>
2024-12-04 13:24:33 +0545

    Completed the services section

--- frontend/src/layout/sections/Services/Services.tsx
+++ frontend/src/layout/sections/Services/Services.tsx
@@ -1,6 +1,8 @@
 import { Button } from "@/components/ui/button";
 import { Heading } from "@/layout/components/Heading";
-import { ArrowLeft, ArrowRight } from "lucide-react";
+import { servicesData } from "./data";
+import { TServicesData } from "./type";
+import { ArrowRight } from "lucide-react";
 
 export default function Services() {
   return (
@@ -22,27 +24,29 @@ export default function Services() {
         </p>
       </div>
       <article className="grid md:grid-rows-2 grid-col-1 md:grid-cols-2 lg:grid-cols-3 gap-8 md:gap-10 lg:gap-12">
-        <ServicesCard />
+        {servicesData.map((service) => (
+          <ServicesCard data={service} />
+        ))}
       </article>
     </section>
   );
 }
 
-function ServicesCard() {
+function ServicesCard({ data }: { data: TServicesData }) {
+  const { icon, title, description } = data;
+
   return (
     <div className="bg-white group py-12 px-8 rounded-[18px] shadow-elevation relative">
       <div className="space-y-6 mb-[58px]">
-        <p className="para-lg font-semibold capitalize">fitness center</p>
-        <p className="para-sm">
-          Achieve your fitness goals with a fully equipped gym for all levels.
-        </p>
+        <p className="para-lg font-semibold capitalize">{title}</p>
+        <p className="para-sm">{description}</p>
       </div>
       <Button variant={"accentUnderline"} className="rounded-none p-0 gap-1">
         get service <ArrowRight strokeWidth={3} />
       </Button>
       <div className="absolute transition-all group-hover:-top-1 left-8 top-0 -translate-y-1/2 bg-secondary size-[60px] rounded-3xl p-2">
         <div className="bg-primary size-full rounded-2xl flex items-center justify-center">
-          <ArrowLeft className="stroke-white" />
+          <img src={icon} alt="An icon featuring each service." />
         </div>
       </div>
     </div>
