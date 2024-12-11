0e89448da2dc6fb42101411e24ba0a9a7ce63a5d
@kenab.kc <kenab.kc@islingtoncollege.edu.np>
2024-12-11 19:01:19 +0545

    Created the about page design

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/layout/sections/Navbar/Navbar";
 import Hero from "@/layout/sections/Hero/Hero";
 import TrustedBy from "@/layout/components/TrustedBy";
 import About from "@/layout/sections/About/About";
+import { About as AboutPage } from "@/pages/website/About";
 import Services from "@/layout/sections/Services/Services";
 import Membership from "@/layout/sections/Membership/Membership";
 import Footer from "@/layout/sections/Footer/Footer";
@@ -16,6 +17,7 @@ export default function App() {
         <Hero />
         <TrustedBy />
         <About />
+        <AboutPage />
         <Services />
         <Membership variant="website" />
         <SuccessStories />
