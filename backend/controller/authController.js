c5e25e74bee95ebbc63bca23ee8ac76ecbf8c51d
@kenab.kc <kenab.kc@islingtoncollege.edu.np>
2025-01-06 08:05:26 +0545

    Set up the backend and integreated forgot password and reset

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -2,6 +2,8 @@ const jwt = require('jsonwebtoken');
 const Maintainer = require('../models/maintainerModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+const sendEmail = require('./../utils/email');
+const crypto = require('crypto');
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -13,6 +15,8 @@ exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await Maintainer.create(req.body);
 
   const token = signToken(newUser._id);
+
+  newUser.password = undefined;
 
   res.status(201).json({ status: 'success', token, data: { user: newUser } });
 });
@@ -26,7 +30,7 @@ exports.login = catchAsync(async (req, res, next) => {
 
   // 2) Check if user exists on the database
   const maintainer = await Maintainer.findOne({ email }).select('+password'); // + to select the field that is deselected on the schema
-  console.log(await maintainer.correctPassword(password, maintainer.password));
+
   if (
     !maintainer ||
     !(await maintainer.correctPassword(password, maintainer.password))
@@ -38,5 +42,74 @@ exports.login = catchAsync(async (req, res, next) => {
   const token = signToken(maintainer._id);
 
   // 4) Send in the response
-  res.status(200).json({ status: 'success', token, data: { user: {} } });
+  maintainer.password = undefined;
+  res.status(200).json({ status: 'success', token, data: { maintainer } });
+});
+
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+  // 1) Get user based on the posted email
+  const maintainer = await Maintainer.findOne({ email: req.body.email });
+
+  if (!maintainer) {
+    return next(new AppError('There is no user with email address.', 404));
+  }
+  // 2) Generate random token
+  const resetToken = maintainer.createPasswordResetToken();
+  await maintainer.save({ validateBeforeSave: false });
+
+  // 3) Send it to the user's email
+  const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/users/resetPassword/${resetToken}`;
+
+  const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to : ${resetUrl}`;
+
+  try {
+    console.log('first');
+    await sendEmail({
+      email: maintainer.email,
+      subject: 'Your password reset',
+      message,
+    });
+    console.log('second');
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Token sent to email',
+    });
+  } catch (err) {
+    maintainer.passwordResetToken = undefined;
+    maintainer.passwordResetExpires = undefined;
+    await maintainer.save({ validateBeforeSave: false });
+    console.log(err);
+
+    return next(
+      new AppError(
+        'There was an error sending the email. Try again later.',
+        500,
+      ),
+    );
+  }
+});
+
+exports.resetPassword = catchAsync(async (req, res, next) => {
+  const hashedToken = crypto
+    .createHash('sha256')
+    .update(req.params.token)
+    .digest('hex');
+
+  const maintainer = await Maintainer.findOne({
+    passwordResetExpires: { $gt: Date.now() },
+    passwordResetToken: hashedToken,
+  });
+
+  if (!maintainer) {
+    return next(new AppError('Token is invalid or has expired'));
+  }
+
+  maintainer.password = req.body.password;
+  maintainer.passwordConfirm = req.body.password;
+  maintainer.passwordResetToken = undefined;
+  maintainer.passwordResetExpires = undefined;
+  await maintainer.save();
+
+  res.status(200).json({ status: 'success' });
 });
