1f289290ae140b73c201c2ad8764e9c56e90488a
@kenab.kc <kenab.kc@islingtoncollege.edu.np>
2025-01-08 21:14:45 +0545

    Fixed the error sent from the backend to the frontend

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,15 @@ const maintainerRouter = require('./routes/maintainerRoutes');
 const authRouter = require('./routes/authRoutes');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controller/errorController');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
+
+app.use(cors({ credentials: true, origin: process.env.CLIENT_URL }));
+
+app.options('*', cors());
 
 // Middleware to get detailed info about the request on the console of the backend server
 console.log(process.env.NODE_ENV);
