545119cb17fbf2f15c52611880c0f6ed63796a20
@kenab.kc <kenab.kc@islingtoncollege.edu.np>
2025-05-29 10:38:01 +0545

    Remove duplicate Login folder files (capital L)

--- frontend/src/system/pages/Login/validator.ts
+++
@@ -1,6 +0,0 @@
-import z from "zod";
-
-export const loginSchema = z.object({
-  email: z.string().email("Please enter a valid email"),
-  password: z.string().nonempty("Please enter your password"),
-});
