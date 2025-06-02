344ad58c48bd8e606008825e467c3a7697dfec62
@kenab.kc <kenab.kc@islingtoncollege.edu.np>
2025-06-02 23:24:12 +0545

    Test 1

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -273,7 +273,6 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
 
 exports.restrictTo = (...roles) => {
   return (req, res, next) => {
-    console.log(req.user.role);
     if (!roles.includes(req.user.role)) {
       return next(
         new AppError('You do not have permission to perform this action', 403),
--- backend/utils/token.js
+++ backend/utils/token.js
@@ -16,10 +16,10 @@ const setupCookie = async (user) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
     ),
     httpOnly: true,
+    sameSite: 'None',
+    secure: true,
   };
-  if (process.env.NODE_ENV === 'production') {
-    cookieOptions.secure = true;
-  }
+
   return { token, cookieOptions };
 };
 
