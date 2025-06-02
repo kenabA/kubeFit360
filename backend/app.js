04ea8b1b59afc24d977d195abc221811a5b06cab
@kenab.kc <kenab.kc@islingtoncollege.edu.np>
2025-06-02 22:34:45 +0545

    Fixed the cors

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: [process.env.CLIENT_URL, process.env.PROD_URL],
+    origin: 'https://kube-fit-360-plum.vercel.app',
     credentials: true,
   }),
 );
